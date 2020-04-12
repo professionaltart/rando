826a7360bc26ed262ea15acc30c82636a404c908
professionaltart <nich@nichers.net>
2020-04-12 17:38:46 +1000

    Update dump.bat
    Clarity/directions

--- dump.bat
+++ dump.bat
@@ -1,4 +1,7 @@
 @echo off
-REM change destination path before use
+REM usage: dump servicename
+REM eg dump glances
+REM 
+REM Remember to change and/or create destination path before use
 
 nssm dump %1 > "E:\backups\nssm settings\%1.bat"
