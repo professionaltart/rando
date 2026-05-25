54b371dacf2e163a3368784e60f82bea00d08f0c
professionaltart <nich@nichers.net>
2026-05-25 16:58:56 +1000

    Add PowerShell script to download and extract CV database

---
+++ mylar/grabLocalCVDB.ps1
@@ -0,0 +1,13 @@
+# Use cURL to grab latest copy of local CV database archive
+## lazy-enablement
+
+$source = "https://mick.nerdfirehurricane.com/localcv/localcv.zip"
+# download to where FakeVine can see it, or a temp location first
+$destination = "F:\data\comics\localcvdb"
+$date = Get-Date -Format "yyyyMMdd"
+
+curl $source -o $destination\localcvdb-$($date).zip
+
+# manually extract archive to make sure dv isn't in use before overtwriting?
+# Confirm switch will prompt user input to extract, and again to overwrite
+Expand-Archive -Path $destination\localcvdb-$($date).zip -DestinationPath $destination -Force -Confirm
