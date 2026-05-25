177b8cb076d1f219bdf5b5ab4542f17664baf3e3
professionaltart <nich@nichers.net>
2026-05-25 17:31:09 +1000

    Genericise

--- dump.bat
+++ dump.bat
@@ -1,9 +1,9 @@
 @echo off
 
-REM Simple Windows batch file to dump service information to re-create using NSSM on a fresh machine/install/etc
+REM Simple Windows batch file to dump service information (to re-create using NSSM on a fresh machine/install/etc)
 REM usage: dump servicename
 REM eg dump glances
 REM 
 REM Remember to change and/or create destination path before use
 
-nssm dump %1 > "E:\backups\nssm settings\%1.bat"
+nssm dump %1 > "P:\ath\to\backups\nssm settings\%1.bat"
