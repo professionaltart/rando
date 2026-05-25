69fedb4b45a7e39791fde58b5ccbbb2afccc2ed8
professionaltart <nich@nichers.net>
2026-05-25 17:44:52 +1000

    Improve comments in CT-fakevine.ps1
    Updated comments for clarity regarding ComicTagger usage.

--- mylar/CT-fakevine.ps1
+++ mylar/CT-fakevine.ps1
@@ -5,12 +5,12 @@
 $fakevine = "http://dockerhost:8463"
 # fakevine APIkey
 $fvApiKey = "Lorem Ipsum"
-# ComicTagger location
+# ComicTagger location - may need to manually specific if below doesn't pick up local/python CT
 $comictagger = (Get-Command comictagger).source
 # path to comic library/files to be processed; --glob allows processing of wildcards here
 $library = "P:\ath\to\comics\"
 
-# 'default' CT flags I've found reliable:
+# 'default' CT flags I've found reliable (assumes you have already run CT at least once and told it where to find rar/unrar files etc)
 & $comictagger -1isofat cr --tags-write cr,cix --use-publisher-filter --source comicvine --comicvine-key $fvApiKey --comicvine-url $fakevine --cv-use-series-start-as-volume --tpb-detection --glob $library
 
 # uses cover hashing - found lots of false matches in the past
