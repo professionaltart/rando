f6c3e8de8da3b0cf4a6f92e2bd7f51b0eb9bf7f8
professionaltart <nich@nichers.net>
2026-05-25 16:22:42 +1000

    Add alternative log checking command as comment
    Added commented-out alternative command for log checking using pure PowerShell.

---
+++ mylarLogs_errors.ps1
@@ -0,0 +1,12 @@
+# Tail Mylar logs (re-use pre-existing function to tail Mylar logs), 
+# looking for CRC or other errors that would stop post-processing.
+
+# set Mylar's log directory:
+$MylarLogs = "C:\programdata\mylar3\logs\mylar.log"
+
+# using ripgrep (highlights selected queries in red text)
+Get-Content -Path $MylarLogs -last 5 -wait  | rg -e 'CRC check failed' -e 'failed condition check'
+
+# using pure pwsh (highlights selected queries with white background)
+## use if RipGrep not installed
+# Get-Content -Path $MylarLogs -last 5 -wait  | sls -pattern 'CRC check failed','failed condition check'
