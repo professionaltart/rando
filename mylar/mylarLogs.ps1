212a697d1d2aafc3517b08a53dcd6b4edb462d2d
professionaltart <nich@nichers.net>
2026-05-25 17:28:04 +1000

    Genericise

--- mylar/mylarLogs.ps1
+++ mylar/mylarLogs.ps1
@@ -2,7 +2,7 @@
 ## (if kept in ~, saves terminal wordiness/having to change to Mylar's dir first)
 
 # set Mylar's log directory:
-$MylarLogs = "C:\programdata\mylar3\logs\mylar.log"
+$MylarLogs = "C:\path\to\mylar3\logs\mylar.log"
 
 # using bat(cat) to prettify/format the output in the terminal 
 Get-Content -Path $MylarLogs -last 5 -wait  | bat --paging=never -l log
